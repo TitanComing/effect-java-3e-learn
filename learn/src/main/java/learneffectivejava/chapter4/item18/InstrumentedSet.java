67cf3a0245d4884a2162e6c606c092a472a69fc5
peng <1740298094@qq.com>
2020-08-12 22:23:21 +0800

    日常工作提交

---
+++ learn/src/main/java/learneffectivejava/chapter4/item18/InstrumentedSet.java
@@ -0,0 +1,38 @@
+package learneffectivejava.chapter4.item18;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by peng on 2020/8/12.
+ */
+public class InstrumentedSet<E> extends ForwardingSet<E> {
+    private int addCount = 0;
+
+    public InstrumentedSet(Set<E> s) {
+        super(s);
+    }
+
+    @Override
+    public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+
+    @Override public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return super.addAll(c);
+    }
+
+    public int getAddCount() {
+        return addCount;
+    }
+
+    public static void main(String[] args) {
+        InstrumentedSet<String> s = new InstrumentedSet<>(new HashSet<>());
+        s.addAll(List.of("Snap", "Crackle", "Pop"));
+        System.out.println(s.getAddCount());
+    }
+}
