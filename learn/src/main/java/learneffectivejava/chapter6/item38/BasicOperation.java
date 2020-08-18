c087270600a94194430e4e9193b777dd4d68de60
peng <renpeng@orientsec.com.cn>
2020-08-18 18:13:01 +0800

    日常工作提交20200818

---
+++ learn/src/main/java/learneffectivejava/chapter6/item38/BasicOperation.java
@@ -0,0 +1,40 @@
+package learneffectivejava.chapter6.item38;
+
+/**
+ * Create by peng on 2020/8/18.
+ */
+public enum BasicOperation implements Operation {
+    PLUS("+") {
+        @Override
+        public double apply(double x, double y) {
+            return x + y;
+        }
+    },
+    MINUS("-") {
+        public double apply(double x, double y) {
+            return x - y;
+        }
+    },
+    TIMES("*") {
+        public double apply(double x, double y) {
+            return x * y;
+        }
+    },
+    DIVIDE("/") {
+        public double apply(double x, double y) {
+            return x / y;
+        }
+    };
+
+
+    private final String symbol;
+
+    BasicOperation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
+}
