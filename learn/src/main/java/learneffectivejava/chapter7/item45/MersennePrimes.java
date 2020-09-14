f74f168dc525543878c41d346442e83e218d2989
peng <renpeng@orientsec.com.cn>
2020-09-14 18:06:56 +0800

    补充了item45中缺失的代码

--- learn/src/main/java/learneffectivejava/chapter7/item45/MersennePrimes.java
+++ learn/src/main/java/learneffectivejava/chapter7/item45/MersennePrimes.java
@@ -3,18 +3,21 @@ package learneffectivejava.chapter7.item45;
 import java.math.BigInteger;
 import java.util.stream.Stream;
 
+import static java.math.BigInteger.ONE;
 import static java.math.BigInteger.TWO;
 
 /**
  * Create by peng on 2020/9/7.
  */
 public class MersennePrimes {
-    static Stream<BigInteger> primes(){
+    static Stream<BigInteger> primes() {
         return Stream.iterate(TWO, BigInteger::nextProbablePrime);
     }
 
-    public static void main(String[] args){
-        primes().map(p -> TWO.pow())
+    public static void main(String[] args) {
+        primes().map(p -> TWO.pow(p.intValueExact()).subtract(ONE))
+                .filter(mersenne -> mersenne.isProbablePrime(50))
+                .limit(20)
+                .forEach(mp -> System.out.println(mp.bitLength() + ":" +mp));
     }
-
 }
